26c07a5c7dca24cd570c2d672473aea6f75551a8
asantos3 <andrewskad@gmail.com>
2017-01-17 01:08:29 +0000

    Implement options

--- README.md
+++ README.md
@@ -9,6 +9,17 @@ We use three VMs with CentOS 7. One with NGINX for load balancing and caching an
 
 Using [sharp](https://github.com/lovell/sharp), a high performance Node.js image processing, the fastest module to resize JPEG, PNG, WebP and TIFF images. Uses the libvips library.
 
+
+### How To
+
+The folder node is for each Node.js server, we can run the server by doing `node node/main.js`.
+
+The folder nginx is for the NGINX, we can run it by doing `sudo service nginx start`:
+* The file default.conf should be `/etc/nginx/conf.d`.
+* The file nginx.conf should be `/etc/nginx/`.
+* Use `setenforce 0` if `service nginx start` gives you an error.
+
+
 ### Features
 
 * [x] Resize images
@@ -16,11 +27,10 @@ Using [sharp](https://github.com/lovell/sharp), a high performance Node.js image
 * [x] Load Balacing
 * [x] Caching
 * [x] Pretty interface and landing page
-* [ ] Watermarking
-* [ ] Rotation
-
-### Notes
-
-The file main.js is for Node.js, the files default.conf and nginx.conf are for NGINX.
-
-Use 'setenforce 0' if 'service nginx restart' gives you an error.
+* [x] Rotation
+* [x] Flip
+* [x] Flop
+* [x] Sharpen
+* [x] Blur
+* [x] Negate
+* [x] Normalize
--- node/main.js
+++ node/main.js
@@ -12,9 +12,13 @@ if (cluster.isMaster) {
   var request = require('request');
   var sharp = require('sharp');
 
+
   var app = express();
+  // use the /images location for the files in www/images
   app.use('/images', express.static(path.join(__dirname, 'www/images')));
+  // use the /images location for the files in www/images
   app.use('/css', express.static(path.join(__dirname, 'www/css')));
+  // use the / location for the files in www
   app.use(express.static(path.join(__dirname, 'www')));
 
   // show index
@@ -31,6 +35,13 @@ if (cluster.isMaster) {
     var url = req.query.url;
     var width = parseInt(req.query.width);
     var height = parseInt(req.query.height);
+    var angle = parseInt(req.query.angle);
+    var flip = req.query.flip;
+    var flop = req.query.flop;
+    var sharpen = req.query.sharpen;
+    var blur = req.query.blur;
+    var negate = req.query.negate;
+    var normalize = req.query.normalize;
 
     // if url, width or height are not defined return resize.html
     if(typeof url==='undefined' || url==='' || 
@@ -38,10 +49,50 @@ if (cluster.isMaster) {
     typeof height==='undefined' || isNaN(height)) {
       res.redirect( "/resize.html" );
     } else {
-      // try to decode the base64 string and present the image
+      // try to it, if it fails return resized.html
       try {
+        // try to decode the base64 string
         var urlDecode = new Buffer(url, 'base64').toString("ascii");
-        request.get(urlDecode).pipe(sharp().resize(width, height)).pipe(res);
+        var image = sharp().resize(width, height);
+
+        // if the user has specified an angle check if it's 0, 90, 180 or 270
+        // then rotate it
+        if(typeof angle!=='undefined' && !isNaN(angle)) {
+          if(angle == 0 || angle == 90 || angle == 180 || angle == 270) {
+            image.rotate(angle);
+          }
+        }
+        // check if the flip was checked
+        // then flip it
+        if(flip == "on") {
+          image.flip();
+        }
+        // check if the flop was checked
+        // then flop it
+        if(flop == "on") {
+          image.flop();
+        }
+        // check if the sharpen was checked
+        // then sharpen it
+        if(sharpen == "on") {
+          image.sharpen();
+        }
+        // check if the blur was checked
+        // then blur it
+        if(blur == "on") {
+          image.blur();
+        }
+        // check if the negate was checked
+        // then negate it
+        if(negate == "on") {
+          image.negate();
+        }
+        // check if the normalize was checked
+        // then normalize it
+        if(normalize == "on") {
+          image.normalize();
+        }
+        request.get(urlDecode).pipe(image).pipe(res);
       }
       catch (e) {
         res.redirect( "/resize.html" );
